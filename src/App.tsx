69efd8de04e3f0b71817714ba567549eb7b30819
Robert <rhskaar@gmail.com>
2022-05-05 20:12:07 +0200

    updated build actions

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi There 
+          Hi There xx
         </p>
         <a
           className="App-link"
