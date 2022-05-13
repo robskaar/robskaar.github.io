7a0e79478af6e78ba1759aa252b78847fa79fdf8
robert <rhskaar@gmail.com>
2022-05-13 23:14:39 +0200

    initial work from react template

---
+++ src/Pages/Landing/Landing.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react";
+import Landing from "./Landing";
+
+test("renders learn react link", () => {
+  render(<Landing />);
+  const linkElement = screen.getByText(/Currently creating my portfolio so stay tuned for changes/i);
+  expect(linkElement).toBeInTheDocument();
+});
---
+++ src/Pages/Landing/Landing.tsx
@@ -0,0 +1,17 @@
+import FooterMenu from "../../Components/Navigation/FooterMenu/FooterMenu";
+import HeaderMenu from "../../Components/Navigation/HeaderMenu/HeaderMenu";
+import PageWrapper from "../../Components/Page/PageWrapper";
+import styles from "./Landing.module.css";
+export default function Landing() {
+  return (
+    <PageWrapper>
+      <HeaderMenu></HeaderMenu>
+      <div className={styles.temptext}>
+        {"Hi, i\'m Robert."}
+        <br/>
+        {"Currently creating my portfolio so stay tuned for changes"}
+      </div>
+      <FooterMenu></FooterMenu>
+    </PageWrapper>
+  );
+}
