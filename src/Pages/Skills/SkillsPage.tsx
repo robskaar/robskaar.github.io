471e5a5ec90236f8071965c39b328cd6279fecf1
robert <rhskaar@gmail.com>
2022-06-07 15:34:02 +0200

    added spa

--- src/Pages/Skills/SkillsPage.tsx
+++ src/Pages/Skills/SkillsPage.tsx
@@ -1,11 +1,9 @@
 import FooterMenu from "../../Common/Components/Navigation/FooterMenu/FooterMenu";
-import HeaderMenu from "../../Common/Components/Navigation/HeaderMenu/HeaderMenu";
 import PageWrapper from "../../Common/Components/Page/PageWrapper";
 import styles from "./SkillsPage.module.css";
 export default function SkillsPage() {
   return (
     <PageWrapper>
-      <HeaderMenu></HeaderMenu>
       <div className={styles.temptext}>
         {"Hi, i'm Robert."}
         <br />
