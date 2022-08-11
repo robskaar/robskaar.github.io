4da872732bdb6edb074ed2a2f62fb7d147049d91
Robert <rhskaar@gmail.com>
2022-08-11 21:46:24 +0200

    fixed some css animations for better practice

--- src/Pages/IndexPage/IndexPage.tsx
+++ src/Pages/IndexPage/IndexPage.tsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import styles from "./IndexPage.module.css";
 export default function IndexPage() {
+  const [timer, setTime] = useState(0);
   return (
     <div className={styles.indexPageWrapper}>
-      <h1 className={styles.h1}>Hi there 
-          <span className="wave">👋</span> </h1>
-      <h2>I'm a web developer combining</h2>
+      <h1 className={styles.h1}>
+        Hi there
+        <span className={styles.wave}>👋</span>
+      </h1>
+      <h2 className={styles.typeFirst}>I'm a web developer</h2>
+      <h3> I combine</h3>
       <div>
         <div>UX</div>
         <div>Accesibility</div>
