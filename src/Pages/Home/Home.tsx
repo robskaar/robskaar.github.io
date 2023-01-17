a41983d4517a4325b8ed4d34d6ac30ab75539334
robert <rhskaar@gmail.com>
2023-01-17 21:17:49 +0100

    updated imports

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,9 +1,6 @@
-import { useEffect, useState } from "react";
-import PhoneAnimation from "../Portfolio/Components/PhoneAnimation/PhoneAnimation";
 import styles from "./Home.module.css";
 
 export default function Home() {
-  const [roaring, setRoaring] = useState<boolean>(false);
   document.title = "Robert Skaar | Home";
   return (
     <div className={styles.indexPageWrapper}>
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import SPAWrapper from "./SPAWrapper/SPAWrapper";
 import "./index.css";
 
