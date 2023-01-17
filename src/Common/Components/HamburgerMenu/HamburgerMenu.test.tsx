f6b63e6280cdd4793fb9656766966ddc121cffd7
robert <rhskaar@gmail.com>
2023-01-17 21:44:36 +0100

    fixed failing unit test

--- src/Common/Components/HamburgerMenu/HamburgerMenu.test.tsx
+++ src/Common/Components/HamburgerMenu/HamburgerMenu.test.tsx
@@ -1,10 +1,12 @@
 import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
 import HamburgerMenu from "./HamburgerMenu";
 
 
 test("renders a clickable hamburger menu in low width viewports", () => {
     global.innerWidth = 500
-  render(<HamburgerMenu />);
+    
+  render(<BrowserRouter><HamburgerMenu /></BrowserRouter>);
   const hamburgerMenu = screen.getByRole("menu");
   expect(hamburgerMenu).toBeInTheDocument();
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import SPAWrapper from "./SPAWrapper/SPAWrapper";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import styles from './index.module.css'
 import "./index.css";
+import Home from "./Pages/Home/Home";
+import Portfolio from "./Pages/Portfolio/Portfolio";
+import HamburgerMenu from "./Common/Components/HamburgerMenu/HamburgerMenu";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -11,7 +14,23 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <SPAWrapper></SPAWrapper>
+    <div
+      className={`${styles.pagewrapper}`}
+    >
+      <nav id={styles.fullmenu}>
+        <div className={styles.nav_menu}>
+          <Link to="/portfolio">portfolio</Link> <Link to="/">blog</Link>
+          <Link to="/">about</Link>
+        </div>
+        <HamburgerMenu></HamburgerMenu>
+      </nav>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home></Home>} />
+          <Route path="/portfolio" element={<Portfolio></Portfolio>} />
+        </Routes>
+      </main>
+    </div>
     </BrowserRouter>
   </React.StrictMode>
 );
